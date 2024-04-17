02ebd7a764e8f6b608dde444f8100c2173a50f86
Diolix <48244032+jbdioli@users.noreply.github.com>
2024-04-17 16:41:48 +0200

    add

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,32 @@
         {
             InitializeComponent();
 
+            UserTheme.SetTheme();
+
             MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            UserTheme.SetTheme();
+            RequestedThemeChanged += App_RequestedThemeChanged;
+        }
+
+        protected override void OnSleep()
+        {
+            UserTheme.SetTheme();
+            RequestedThemeChanged += App_RequestedThemeChanged;
+        }
+
+        protected override void OnResume()
+        {
+            UserTheme.SetTheme();
+            RequestedThemeChanged += App_RequestedThemeChanged;
+        }
+
+        private static void App_RequestedThemeChanged(object? sender, AppThemeChangedEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(UserTheme.SetTheme);
         }
     }
 }
